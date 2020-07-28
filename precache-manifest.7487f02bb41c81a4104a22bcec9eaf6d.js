abe4299588e61f8a897d06c5a28056b95309b247
Polina Slyusareva <ghileors@gmail.com>
2020-07-28 15:47:47 +0300

    Updates

---
+++ precache-manifest.7487f02bb41c81a4104a22bcec9eaf6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83988fb0ddd9988a9dee8e7c33567ebf",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "477d6fe68fba1c0549c2",
+    "url": "/goit-react-hw-01-components/static/js/2.c6a339f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.c6a339f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6432cc59dcab3e548127",
+    "url": "/goit-react-hw-01-components/static/js/main.1c8cde58.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);

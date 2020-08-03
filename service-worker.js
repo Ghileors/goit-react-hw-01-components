0751b9298fd314b4db39f787ddb82de80da48b9e
Polina Slyusareva <ghileors@gmail.com>
2020-08-03 20:08:14 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.50fc009cfcc0e1808c704fe4adde1dce.js"
+  "/goit-react-hw-01-components/precache-manifest.5b53e03527d94d96fd2194060b9856ff.js"
 );
 
 self.addEventListener('message', (event) => {
